2d46c1fc1df152424bde83e286087bd3bac261d6
Keita Kobayashi <keita@kbys.me>
2016-01-01 15:08:50 +0900

    Simplify Exfile.Phoenix.Helpers.file_path/2

--- config/dev.exs
+++ config/dev.exs
@@ -1,2 +1,4 @@
 use Mix.Config
 
+config :exfile, Exfile,
+  secret: "donttellanyone"
--- lib/exfile/phoenix/helpers.ex
+++ lib/exfile/phoenix/helpers.ex
@@ -1,13 +1,10 @@
 defmodule Exfile.Phoenix.Helpers do
   alias Exfile.Token
 
-  def file_path(%Exfile.File{} = file) do
-    path = "#{file.backend.backend_name}/#{file.id}/file"
-    "/attachments/" <> Token.build_path(path)
-  end
-
-  def file_path(%Exfile.File{} = file, format: format) do
-    path = "#{file.backend.backend_name}/#{file.id}/file.#{format}"
-    "/attachments/" <> Token.build_path(path)
+  def file_path(%Exfile.File{} = file, opts \\ []) do
+    format = Dict.get(opts, :format)
+    format_ext = if format, do: "." <> format
+    path = [file.backend.backend_name, file.id, "file#{format_ext}"]
+    "/attachments/" <> (Enum.join(path, "/") |> Token.build_path)
   end
 end
