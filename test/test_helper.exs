130dfd2e03dc6036722f04dcdbb4a0c455f404e9
Keita Kobayashi <keita@kbys.me>
2016-01-04 20:09:35 +0900

    Fix test failures caused by lazy initialization of FileSystem backend

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,3 +1,10 @@
+# Clean out the temporary files path to ensure consistent tests.
+File.rm_rf!(Path.expand("./tmp"))
+
+# Prepare tmp/cache because we'll be writing files there before the backend is
+# initialized.
+File.mkdir_p!(Path.expand("./tmp/cache"))
+
 ExUnit.start()
 
 Exfile.ProcessorRegistry.register "reverse", Exfile.ReverseProcessor
