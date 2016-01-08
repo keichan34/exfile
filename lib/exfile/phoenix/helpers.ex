ef94473d0e76be1932f8170e703af317ce77ec02
Keita Kobayashi <keita@kbys.me>
2016-01-08 15:55:25 +0900

    Support for processor and processor_args in the file_path helper #2

--- lib/exfile/phoenix/helpers.ex
+++ lib/exfile/phoenix/helpers.ex
@@ -2,9 +2,24 @@ defmodule Exfile.Phoenix.Helpers do
   alias Exfile.Token
 
   def file_path(%Exfile.File{} = file, opts \\ []) do
+    path = [file.backend.backend_name]
+
+    case Dict.fetch(opts, :processor) do
+      {:ok, processor} ->
+        path = path ++ [processor]
+      :error -> nil
+    end
+
+    case Dict.fetch(opts, :processor_args) do
+      {:ok, processor_args} ->
+        path = path ++ processor_args
+      :error -> nil
+    end
+
     format = Dict.get(opts, :format)
     format_ext = if format, do: "." <> format
-    path = [file.backend.backend_name, file.id, "file#{format_ext}"]
+    path = path ++ [file.id, "file#{format_ext}"]
+
     "/attachments/" <> (Enum.join(path, "/") |> Token.build_path)
   end
 end
