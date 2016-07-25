24ccf9520be726b469bfcf8372edff23ea45d40d
Alex Antonov <alex@fasteria.ru>
2016-07-25 16:22:26 +0300

    Cast content_type ability

---
+++ lib/exfile/ecto/cast_content_type.ex
@@ -0,0 +1,31 @@
+if Code.ensure_loaded?(Ecto) do
+
+defmodule Exfile.Ecto.CastContentType do
+  alias Ecto.Changeset
+  alias Exfile.Processor.ContentType, as: ContentTypeProcessor
+
+  def cast_content_type(changeset, field) when is_atom(field) do
+    cast_content_type(changeset, field, String.to_atom("#{field}_content_type"))
+  end
+  def cast_content_type(changeset, field, content_type_field) when is_atom(content_type_field) do
+    case Changeset.get_change(changeset, field) do
+      %Exfile.File{} -> perform_cast(changeset, field, content_type_field)
+      _              -> changeset
+    end
+  end
+
+  defp perform_cast(changeset, field, content_type_field) do
+    changeset
+    |> Changeset.get_change(field)
+    |> ContentTypeProcessor.call([], [])
+    |> case do
+      { :ok, processed_file } ->
+        changeset
+        |> Changeset.put_change(field, processed_file)
+        |> Changeset.put_change(content_type_field, processed_file.meta["content_type"])
+      _ -> changeset
+    end
+  end
+end
+
+end
---
+++ test/exfile/ecto/cast_content_type_test.exs
@@ -0,0 +1,47 @@
+defmodule Exfile.Ecto.CastContentTypeTest do
+  use ExUnit.Case, async: true
+
+  import Ecto.Changeset, only: [cast: 3]
+  import Exfile.Ecto.CastContentType
+
+  test "assigns content type correctly" do
+    changeset = cast(initial_changeset, %{ image: image_file }, [:image])
+      |> cast_content_type(:image)
+
+    assert changeset.changes[:image_content_type] == "image/jpeg"
+  end
+
+  test "doesn't assign anything if file is not present in changeset" do
+    changeset = cast(initial_changeset, %{ image: nil }, [:image])
+      |> cast_content_type(:image)
+
+    assert changeset.changes[:image_content_type] == nil
+  end
+
+  test "ability to use custom field name" do
+    changeset = cast(initial_changeset, %{ image: image_file }, [:image])
+      |> cast_content_type(:image, :image_custom_type)
+
+    assert changeset.changes[:image_custom_type] == "image/jpeg"
+  end
+
+  defp initial_changeset do
+    data  = %{
+      image: nil,
+      image_content_type: nil,
+      image_custom_type: nil
+    }
+
+    types = %{
+      image: Exfile.Ecto.File,
+      image_content_type: :string,
+      image_custom_type: :string
+    }
+
+    { data, types }
+  end
+
+  defp image_file do
+    %Plug.Upload{ path: "test/fixtures/sample.jpg", filename: "sample.jpg" }
+  end
+end
