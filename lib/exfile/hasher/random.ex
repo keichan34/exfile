3b11ded30d31e5293322d06042440b24976c9e3e
Alex Antonov <alex@fasteria.ru>
2016-08-03 07:46:24 +0300

    Remove deprecation

--- lib/exfile/hasher/random.ex
+++ lib/exfile/hasher/random.ex
@@ -2,6 +2,6 @@ defmodule Exfile.Hasher.Random do
   @behaviour Exfile.Hasher
 
   def hash(_uploadable) do
-    :crypto.rand_bytes(30) |> Base.encode16(case: :lower)
+    :crypto.strong_rand_bytes(30) |> Base.encode16(case: :lower)
   end
 end
