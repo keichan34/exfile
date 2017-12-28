d8d451da92755774c6c1ec6f6b0f538e4ed0782e
Carsten Block <mail@carstenblock.org>
2017-12-29 00:38:12 +0100

    Gracefully handle optional exfile fields not being set

--- lib/exfile/ecto.ex
+++ lib/exfile/ecto.ex
@@ -24,26 +24,33 @@ defmodule Exfile.Ecto do
   """
   def prepare_uploads(changeset, fields) do
     Changeset.prepare_changes(changeset, fn changeset ->
-      perform_uploads!(changeset, fields)
+      perform_uploads(changeset, fields)
     end)
   end
 
-  defp perform_uploads!(changeset, fields) do
-    Enum.reduce fields, changeset, fn
-      (field, changeset) ->
-        t = ecto_type_for_field(changeset.data, field)
-        file = Changeset.get_field(changeset, field)
-        case t.upload!(file) do
-          {:ok, uploaded_file} ->
-            Changeset.put_change(changeset, field, uploaded_file)
-          error ->
-            throw error
-        end
+  defp perform_uploads(changeset, fields) do
+    Enum.reduce fields, changeset, fn (field, changeset) ->
+      perform_upload(changeset, field)
     end
   end
 
+  defp perform_upload(changeset, field) do
+    with {:ok, t} <- ecto_type_for_field(changeset.data, field),
+      file when not is_nil(file) <- Changeset.get_field(changeset, field),
+      {:ok, uploaded_file} <- t.upload!(file)
+    do
+      Changeset.put_change(changeset, field, uploaded_file)
+    else
+      nil -> #happens if an exfile field is optional and thus might not be set 
+        changeset
+      _ ->
+        throw "Upload failed"
+    end
+
+  end
+
   defp ecto_type_for_field(%{__struct__: mod}, field) do
-    mod.__schema__(:type, field)
+    {:ok, mod.__schema__(:type, field)}
   end
 end
 
