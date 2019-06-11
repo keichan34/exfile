15a0002d40ece8807b2ca743e44bea43d7db0fa2
Denis Tataurov <denis@grabr.io>
2019-06-11 13:19:22 +0300

    Update deps

--- mix.exs
+++ mix.exs
@@ -55,10 +55,10 @@ defmodule Exfile.Mixfile do
   defp deps do
     [
       {:plug, "~> 1.0"},
-      {:ecto, "~> 1.0 or ~> 2.0", optional: true},
-      {:phoenix, "~> 1.1 or ~> 1.3.0-rc", optional: true},
+      {:ecto_sql, "~> 3.0", optional: true},
+      {:phoenix, "~> 1.1", optional: true},
       {:phoenix_html, "~> 2.3", optional: true},
-      {:poison, "~> 1.5 or ~> 2.0 or ~> 3.1", optional: true},
+      {:poison, "~> 3.0", optional: true},
       {:timex, "~> 2.0", only: [:dev, :test]},
       {:postgrex, "~> 0.11", only: [:dev, :test]},
       {:earmark, "~> 1.1", only: :dev},
