6f99f6e2ffd1a65401e726cebf8936f8d9c34186
Sreenadh TC <kesav.tc8@gmail.com>
2021-09-30 08:34:03 +0530

    fix failing tests

--- test/support/repo.ex
+++ test/support/repo.ex
@@ -1,3 +1,3 @@
 defmodule Exfile.Repo do
-  use Ecto.Repo, otp_app: :exfile
+  use Ecto.Repo, otp_app: :exfile, adapter: Ecto.Adapters.Postgres
 end
--- test/support/s/image.ex
+++ test/support/s/image.ex
@@ -2,7 +2,9 @@ defmodule Exfile.S.Image do
   use Ecto.Schema
 
   schema "images" do
-    field :image, Exfile.Ecto.File
-    field :image_content_type, :string
+    field(:image, Exfile.Ecto.File)
+    field(:image_content_type, :string)
+    field(:image_filename, :string)
+    field(:image_custom_filename, :string)
   end
 end
